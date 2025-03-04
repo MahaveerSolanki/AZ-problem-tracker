087a609f259d80cf36da062a8ba0a2924fc02784
MahaveerSolanki <mahaveersolanki9828@kgpian.iitkgp.ac.in>
2025-03-04 11:16:35 +0530

    Added bookmark button on webpage

--- content.js
+++ content.js
@@ -1,7 +1,27 @@
-// content.js
-// Author:
-// Author URI: https://
-// Author Github URI: https://www.github.com/
-// Project Repository URI: https://github.com/
-// Description: Handles all the webpage level activities (e.g. manipulating page data, etc.)
-// License: MIT
+const bookmarkIconUrl = chrome.runtime.getURL("assets/bookmark.png");
+// console.log("Bookmark Icon URL: ", bookmarkIconUrl);
+
+window.addEventListener("load", addBookMarkButton);
+
+function addBookMarkButton() {
+    console.log("Adding Bookmark Button");
+    const bookmarkButton = document.createElement("img");
+    bookmarkButton.id = "bookmark_Button";
+    bookmarkButton.src = bookmarkIconUrl;
+    bookmarkButton.style.height = "20px";
+    bookmarkButton.style.width = "20px";
+    bookmarkButton.style.cursor = "pointer";
+    
+    const ask_doubt_button = document.getElementsByClassName("coding_ask_doubt_button__FjwXJ")[0];
+
+    if (ask_doubt_button) {
+        ask_doubt_button.parentNode.insertAdjacentElement("afterEnd", bookmarkButton);
+        // console.log("Bookmark Button added.");
+    } else {
+        console.error("Element with class 'coding_ask_doubt_button__FjwXJ' not found.");
+    }
+    bookmarkButton.addEventListener("click", () => {
+        alert("Bookmark Button clicked.");
+    });
+
+}
